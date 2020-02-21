1d7e8e63891540c9aa09e4d1eb296cb29c1d9b7f
SSep <ssep4u@gmail.com>
2020-02-21 11:20:57 +0900

    coupon/views add_coupon(request)

--- coupon/views.py
+++ coupon/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .forms import AddCouponForm
+from .models import Coupon
+
+
+@require_POST
+def add_coupon(request):
+  now = timezone.now()
+  form = AddCouponForm(request.POST)
+  if form.is_valid():
+    code = form.cleaned_data['code']
+
+    try:
+      coupon = Coupon.objects.get(code__iexact=code, use_from__lte=now, use_to__gte=now, active=True)
+      request.session['coupon_id'] = coupon.id
+    except Coupon.DoesNotExist:
+      request.session['coupon_id'] = None
+  return redirect('cart:detail')
