aae7012ebe7fdb1240a7079e7d4df270afc1e5d3
SSep <ssep4u@gmail.com>
2020-02-29 11:40:28 +0900

    cart/context_processors config/settings shop/views

--- shop/views.py
+++ shop/views.py
@@ -14,7 +14,8 @@ def product_in_category(request, category_slug=None):
   if category_slug:
     current_category = get_object_or_404(Category, slug=category_slug)
     products = products.filter(category=current_category)
-  return render(request, 'shop/list.html', {'current_category': current_category, 'categories': categories, 'products': products, 'cart':cart})
+  return render(request, 'shop/list.html',
+                {'current_category': current_category, 'categories': categories, 'products': products})
 
 
 def product_detail(request, id, product_slug=None):
